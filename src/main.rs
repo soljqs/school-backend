3d36c735d252803ce5b0fcdee7dc1b7cb94b2b60
soljqs <sol2129948540@163.com>
2026-02-06 14:14:45 +0800

    fix render port

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 use actix_web::{App, HttpServer};
-use utoipa::OpenApi;
 use utoipa::Modify;
 use utoipa_swagger_ui::SwaggerUi;
-
+use utoipa::OpenApi;
 mod auth;
 mod students;
 mod swagger;
@@ -14,8 +13,17 @@ use openapi_security::SecurityAddon;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+
+    // ⭐ Render PORT
+    let port: u16 = std::env::var("PORT")
+        .unwrap_or_else(|_| "8080".to_string())
+        .parse()
+        .expect("PORT must be number");
+
     let mut openapi = ApiDoc::openapi();
     SecurityAddon.modify(&mut openapi);
+
+    println!("Server running on 0.0.0.0:{}", port);
 
     HttpServer::new(move || {
         App::new()
@@ -27,7 +35,7 @@ async fn main() -> std::io::Result<()> {
                     .url("/api-doc/openapi.json", openapi.clone()),
             )
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", port))?   // ⭐ 必须
     .run()
     .await
 }
